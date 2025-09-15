88513f431e55816462224db4f91ef3f07f6d7016
theelgreco <stelios.kakoliris@yahoo.com>
2025-09-15 20:05:17 +0100

    adds shadcn

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,13 @@
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Toaster richColors position="top-right" expand />
+      </body>
     </html>
   );
 }
